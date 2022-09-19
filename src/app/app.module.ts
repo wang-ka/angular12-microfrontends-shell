d984f6bb05eb7460754e03993bf3d65c8b8ec32b
wangka <wangka.web@foxmail.com>
2022-09-19 23:51:21 +0800

    feat: 初始化微前端逻辑

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MicrofrontendService } from './microfrontends/microfrontend.service';
 
+export function initializeApp(
+  mfService: MicrofrontendService
+): () => Promise<void> {
+  return () => mfService.initialise();
+}
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    MicrofrontendService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      multi: true,
+      deps: [MicrofrontendService],
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
